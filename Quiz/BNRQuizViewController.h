5aac7b0d9fad7260361369ad47718acff8179d00
Keith Samson <k.samson@klab.com>
2014-06-13 17:18:42 +0800

    Initial commit

---
+++ Quiz/BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by Keith Samson on 6/13/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
---
+++ Quiz/Quiz/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Quiz
+//
+//  Created by Keith Samson on 6/13/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
