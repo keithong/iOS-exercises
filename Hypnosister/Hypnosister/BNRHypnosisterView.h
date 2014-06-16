af84b1d90df6f81b265105373f9c9eb98ab042c1
Keith Samson <k.samson@klab.com>
2014-06-16 16:55:26 +0800

    Hypnosister's first commit

---
+++ Hypnosister/Hypnosister/BNRHypnosisterView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisterView.h
+//  Hypnosister
+//
+//  Created by Keith Samson on 6/16/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisterView : UIView
+
+@end
---
+++ Hypnosister/Hypnosister/BNRHypnosisterView.m
@@ -0,0 +1,31 @@
+//
+//  BNRHypnosisterView.m
+//  Hypnosister
+//
+//  Created by Keith Samson on 6/16/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#import "BNRHypnosisterView.h"
+
+@implementation BNRHypnosisterView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
