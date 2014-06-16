df43ea15f46a26d080c27a309f30a492b15a466c
Keith Samson <k.samson@klab.com>
2014-06-16 11:07:43 +0800

    RandomItems' initial commit

---
+++ RandomItems/RandomItems/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Keith Samson on 6/16/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!\n This is the initial commit!\n");
+        
+    }
+    return 0;
+}
+
