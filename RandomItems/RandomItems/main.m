1a8ab3db0b0226d7125a574b4a58306a730eadb9
Keith Samson <k.samson@klab.com>
2014-06-16 11:14:27 +0800

    Created and populated the mutable array

--- RandomItems/RandomItems/main.m
+++ RandomItems/RandomItems/main.m
@@ -12,9 +12,22 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+
+        // Create a mutable array object, store its address in items variable
+        NSMutableArray *items = [[NSMutableArray alloc]init];
         
-        // insert code here...
-        NSLog(@"Hello, World!\n This is the initial commit!\n");
+        // Send the message addObject: to the NSMutableArray pointed to
+        // by the variable items, passing a string each time
+        
+        [items addObject:@"One"];
+        [items addObject:@"Two"];
+        [items addObject:@"Three"];
+        
+        // Send another message, insertObject:atIndext:, to that same array object
+        [items insertObject:@"Zero" atIndex:0];
+        
+        // Destroy the mutable array object
+        items = nil;
         
     }
     return 0;
