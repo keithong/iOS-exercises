6f56e1b8ea19341a9aa7636cb3383a888f6b6de3
Keith Samson <k.samson@klab.com>
2014-06-17 17:26:10 +0800

    You can now add items in the table

--- Homepwner/BNRItemsViewController.m
+++ Homepwner/BNRItemsViewController.m
@@ -75,6 +75,18 @@
 
 -(IBAction)addNewitem:(id)sender
 {
+    
+    // Create a new BNRItem and add it to the store
+    BNRItem *newItem = [[BNRItemStore sharedStore]createItem];
+    
+    // Figure out where that item is in the array
+    NSInteger lastRow = [[[BNRItemStore sharedStore] allItems] indexOfObject:newItem];
+
+    NSIndexPath *indexPath = [NSIndexPath indexPathForItem:lastRow inSection:0];
+    
+    // Insert this new row into the table
+    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationTop];
+    
 }
 
 -(IBAction)toggleEditingMode:(id)sender
