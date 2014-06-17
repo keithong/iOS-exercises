efe2667e1f2138d3ca234a2365aacc2d7a918567
Keith Samson <k.samson@klab.com>
2014-06-17 10:29:32 +0800

    HypnoNerd's first commit

---
+++ HypnoNerd/BNRHypnosisViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisViewController.h
+//  HypnoNerd
+//
+//  Created by Keith Samson on 6/17/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRHypnosisViewController : NSObject
+
+@end
---
+++ HypnoNerd/BNRHypnosisViewController.m
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisViewController.m
+//  HypnoNerd
+//
+//  Created by Keith Samson on 6/17/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#import "BNRHypnosisViewController.h"
+
+@implementation BNRHypnosisViewController
+
+@end
