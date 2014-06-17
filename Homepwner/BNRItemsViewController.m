5da839ff8149e912914b7a87b9529dbd5eb79441
Keith Samson <k.samson@klab.com>
2014-06-17 15:48:08 +0800

    Homepwner's initial commit

---
+++ Homepwner/BNRItemsViewController.m
@@ -0,0 +1,13 @@
+//
+//  BNRItemsViewController.m
+//  Homepwner
+//
+//  Created by Keith Samson on 6/17/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#import "BNRItemsViewController.h"
+
+@implementation BNRItemsViewController
+
+@end
---
+++ Homepwner/Homepwner/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Homepwner
+//
+//  Created by Keith Samson on 6/17/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
