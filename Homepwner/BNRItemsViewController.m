32a1047b0b57d9f98f4ab0474b74eba0f29c344d
Keith Samson <k.samson@klab.com>
2014-06-18 08:45:57 +0800

    You can now delete items

--- Homepwner/BNRItemsViewController.m
+++ Homepwner/BNRItemsViewController.m
@@ -117,4 +117,21 @@
     }
     return _headerView;
 }
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    // If the table view is asking to commit a delete command:
+    if(editingStyle == UITableViewCellEditingStyleDelete) {
+    
+        NSArray *items = [[BNRItemStore sharedStore] allItems];
+        BNRItem *item = items[indexPath.row];
+        [[BNRItemStore sharedStore] removeItem:item];
+        
+        // Also remove that row from the table view with an animation
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    
+    }
+
+}
 @end
--- Homepwner/Homepwner/BNRItemStore.m
+++ Homepwner/Homepwner/BNRItemStore.m
@@ -58,4 +58,8 @@
     [self.privateItems addObject:item];
     return item;
 }
+-(void)removeItem:(BNRItem *)item
+{
+    [self.privateItems removeObjectIdenticalTo:item];
+}
 @end
