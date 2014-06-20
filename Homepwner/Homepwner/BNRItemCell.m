366db46fac274311b894739f5b694d2dc00724b7
Keith Samson <k.samson@klab.com>
2014-06-20 11:57:56 +0800

    Dynamic Type for items view is now live

--- Homepwner/Homepwner/BNRItemCell.m
+++ Homepwner/Homepwner/BNRItemCell.m
@@ -8,6 +8,13 @@
 
 #import "BNRItemCell.h"
 
+@interface BNRItemCell()
+
+@property (nonatomic, weak) IBOutlet NSLayoutConstraint *imageViewHeightConstraint;
+@property (nonatomic, weak) IBOutlet NSLayoutConstraint *imageViewWidthConstraint;
+
+@end
+
 @implementation BNRItemCell
 
 -(IBAction)showImage:(id)sender
@@ -16,4 +23,52 @@
         self.actionBlock();
     }
 }
+
+-(void)updateInterfaceForDynamicTypeSize
+{
+    UIFont *font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.nameLabel.font = font;
+    self.serialLabel.font = font;
+    self.valueLabel.font = font;
+    
+    static NSDictionary *imageSizeDictionary;
+    
+    if(!imageSizeDictionary){
+        imageSizeDictionary = @{
+                                UIContentSizeCategoryExtraSmall: @40,
+                                UIContentSizeCategorySmall: @40,
+                                UIContentSizeCategoryMedium: @40,
+                                UIContentSizeCategoryLarge: @40,
+                                UIContentSizeCategoryExtraLarge: @45,
+                                UIContentSizeCategoryExtraExtraLarge: @55,
+                                UIContentSizeCategoryExtraExtraExtraLarge: @65
+                                };
+    }
+    
+    NSString *userSize = [[UIApplication sharedApplication] preferredContentSizeCategory];
+    
+    NSNumber *imageSize = imageSizeDictionary[userSize];
+    self.imageViewHeightConstraint.constant = imageSize.floatValue;
+    self.imageViewWidthConstraint.constant = imageSize.floatValue;
+    
+}
+
+-(void)awakeFromNib
+{
+    [super awakeFromNib];
+    [self updateInterfaceForDynamicTypeSize];
+    
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+    [nc addObserver:self
+           selector:@selector(updateInterfaceForDynamicTypeSize)
+               name:UIContentSizeCategoryDidChangeNotification
+             object:nil];
+}
+
+-(void)dealloc
+{
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+    [nc removeObserver:self];
+}
+
 @end
