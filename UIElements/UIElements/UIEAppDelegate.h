15a45d7be71ce429b2883cdac5540a370a3420e5
Keith Samson <k.samson@klab.com>
2014-06-23 14:10:51 +0800

    UIElements' first commit

---
+++ UIElements/UIElements/UIEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UIEAppDelegate.h
+//  UIElements
+//
+//  Created by Keith Samson on 6/23/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
