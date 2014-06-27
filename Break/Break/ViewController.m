c108244c8c564a184976b47f66d363e5579cab0c
Keith Samson <k.samson@klab.com>
2014-06-27 13:42:40 +0800

    Break's first commit

---
+++ Break/Break/ViewController.m
@@ -0,0 +1,51 @@
+//
+//  ViewController.m
+//  Break
+//
+//  Created by Keith Samson on 6/27/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "MyScene.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Configure the view.
+    SKView * skView = (SKView *)self.view;
+    skView.showsFPS = YES;
+    skView.showsNodeCount = YES;
+    
+    // Create and configure the scene.
+    SKScene * scene = [MyScene sceneWithSize:skView.bounds.size];
+    scene.scaleMode = SKSceneScaleModeAspectFill;
+    
+    // Present the scene.
+    [skView presentScene:scene];
+}
+
+- (BOOL)shouldAutorotate
+{
+    return NO;
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return UIInterfaceOrientationMaskAllButUpsideDown;
+    } else {
+        return UIInterfaceOrientationMaskAll;
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+@end
