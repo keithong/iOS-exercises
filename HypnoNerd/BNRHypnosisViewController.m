df2a5ec8f8c6c1b5de75658d4acfee05e107c19e
Keith Samson <k.samson@klab.com>
2014-07-01 11:01:17 +0800

    Added animations to HypnoNerd project

--- HypnoNerd/BNRHypnosisViewController.m
+++ HypnoNerd/BNRHypnosisViewController.m
@@ -10,7 +10,7 @@
 #import "BNRHypnosisterView.h"
 
 @interface BNRHypnosisViewController() <UITextFieldDelegate>
-
+@property (nonatomic, weak) UITextField *textField;
 @end
 
 @implementation BNRHypnosisViewController
@@ -21,7 +21,8 @@
     BNRHypnosisterView *backgroundView = [[BNRHypnosisterView alloc]initWithFrame:frame];
     
     // Create a text field
-    CGRect textFieldRect = CGRectMake(40,70,240,30);
+    CGRect textFieldRect = CGRectMake(40,-30,240,30);
+    
     UITextField *textField = [[UITextField alloc]initWithFrame:textFieldRect];
     
     // Setting the border style on the text field will allow us to see it more easily
@@ -35,7 +36,9 @@
     [backgroundView addSubview:textField];
     
     // Set it as the view of this view controller
+    self.textField = textField;
     self.view = backgroundView;
+    
 }
 
 -(BOOL)textFieldShouldReturn:(UITextField *)textField
@@ -52,7 +55,7 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     
     if(self) {
-    
+        
         // Set the tab bar item's title
         self.tabBarItem.title = @"Hypnotize";
         
@@ -76,7 +79,7 @@
 
 -(void)drawHypnoticMessage:(NSString *)message
 {
-
+    
     for (int i=0; i < 20; i++) {
         UILabel *messageLabel = [[UILabel alloc]init];
         
@@ -105,6 +108,33 @@
         // Add the label to the hierarchy
         [self.view addSubview:messageLabel];
         
+        // Set the label's initial alpha
+        messageLabel.alpha = 0.0;
+        
+        // Animate the alpha to 1.0
+        [UIView animateWithDuration:.5
+                              delay: 0.0
+                            options:UIViewAnimationOptionCurveEaseIn
+                         animations:^{
+                             messageLabel.alpha = 1.0;
+                         }
+                         completion:NULL ];
+        
+        [UIView animateKeyframesWithDuration:1.0 delay:0.0 options:0 animations:^{
+            [UIView addKeyframeWithRelativeStartTime:0 relativeDuration:0.8 animations:^{messageLabel.center = self.view.center;
+            }];
+            
+            [UIView addKeyframeWithRelativeStartTime:0.0 relativeDuration:0.2 animations:^{
+                int x = arc4random() % width;
+                int y = arc4random() % height;
+                messageLabel.center = CGPointMake(x, y);
+            }];
+            
+            
+        } completion:^(BOOL finished){
+            NSLog(@"Animation finished");
+        }];
+        
         UIInterpolatingMotionEffect *motionEffect;
         motionEffect = [[UIInterpolatingMotionEffect alloc]initWithKeyPath:@"center.x" type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
         
@@ -116,9 +146,27 @@
         motionEffect.minimumRelativeValue = @-25;
         motionEffect.maximumRelativeValue = @25;
         [messageLabel addMotionEffect:motionEffect];
-    
+        
     }
     
+    [self.textField resignFirstResponder];
+    
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [UIView animateWithDuration:2.0
+                          delay:0.0
+         usingSpringWithDamping:.25
+          initialSpringVelocity:0.0
+                        options:0
+                     animations:^{
+                         CGRect frame = CGRectMake(40, 70, 240, 30);
+                         self.textField.frame = frame;
+                     }
+                     completion:NULL];
 }
 
 @end
