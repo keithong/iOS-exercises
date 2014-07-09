dd44973b2fe8f48a586ef3bab32ef46262f6163a
Keith Samson <k.samson@klab.com>
2014-07-09 17:26:19 +0800

    added a sample

--- Sample/Sample/GameViewController.m
+++ Sample/Sample/GameViewController.m
@@ -7,14 +7,14 @@
 //
 
 #import "GameViewController.h"
-
 @interface GameViewController ()
+
 @end
 
 @implementation GameViewController
-
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
+    
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
@@ -26,10 +26,8 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
-    
-    [self createBlock];
-    
+    [self animatePlayer:[self player]];
+    [self animateBlock:[self createBlock]];
 }
 
 - (void)didReceiveMemoryWarning
@@ -38,17 +36,73 @@
     // Dispose of any resources that can be recreated.
 }
 
--(void)createBlock
+-(UIView *)player
 {
-    int bottomScreen = [UIScreen mainScreen].bounds.size.height;
-    int blockHeight = 110;
+    int screenHeight = [UIScreen mainScreen].bounds.size.height;
+    int screenWidth = [UIScreen mainScreen].bounds.size.width;
+    
+    int playerHeight = 90;
+    int playerWidth = 25;
+    
+    UIView *player = [[UIView alloc]init];
+    
+    [player setFrame:CGRectMake(0, screenHeight - playerHeight, playerWidth, playerHeight)];
+    player.backgroundColor = [UIColor greenColor];
+    
+    [self.view addSubview:player];
+    return player;
+    
+}
+
+-(UIView *)createBlock
+{
+    int screenHeight = [UIScreen mainScreen].bounds.size.height;
+    int screenWidth = [UIScreen mainScreen].bounds.size.width;
+    
+    int blockHeight = screenHeight/2;
     int blockWidth = 50;
     
     UIView *block = [[UIView alloc]init];
     
-    [block setFrame:CGRectMake(CGRectGetMidX([[UIScreen mainScreen] bounds]), bottomScreen - blockHeight, blockWidth, blockHeight)];
-    block.backgroundColor = [UIColor redColor];
+    [block setFrame:CGRectMake((screenWidth + blockWidth) - blockWidth, screenHeight - blockHeight, blockWidth, blockHeight)];
+    block.backgroundColor = [UIColor grayColor];
+    
+    
     [self.view addSubview:block];
+    return block;
+}
+
+-(void)animateBlock:(UIView *)block
+{
+    int screenWidth = [UIScreen mainScreen].bounds.size.width;
+    int blockWidth = 50;
+    CGRect moveBlock = block.frame;
+    moveBlock.origin.x -= screenWidth + blockWidth;
+    
+    [UIView animateWithDuration:2.0
+                          delay:.1
+                        options: UIViewAnimationOptionRepeat
+                     animations:^{
+                         block.frame = moveBlock;
+                         NSLog(@"%d",screenWidth);
+                     }
+                     completion:^(BOOL finished) {
+                     }
+     ];
+}
+
+-(void)animatePlayer:(UIView *)player
+{
+    int screenWidth = [UIScreen mainScreen].bounds.size.width;
+    int playerWidth = 25;
+    CGRect movePlayer = player.frame;
+    movePlayer.origin.x += screenWidth - 20;
+    [UIView animateWithDuration:6.0
+                     animations:^{
+                         player.frame = movePlayer;
+                         NSLog(@"%d",screenWidth);
+                     }
+     ];
 }
 /*
  #pragma mark - Navigation
