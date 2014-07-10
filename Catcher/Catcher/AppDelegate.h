18ce25f753b3c5805df25c58f5181c88c0d48094
Keith Samson <k.samson@klab.com>
2014-07-10 15:49:56 +0800

    Added another sample

---
+++ Catcher/Catcher/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Catcher
+//
+//  Created by Keith Samson on 7/10/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Catcher/CatcherTests/CatcherTests.m
@@ -0,0 +1,34 @@
+//
+//  CatcherTests.m
+//  CatcherTests
+//
+//  Created by Keith Samson on 7/10/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CatcherTests : XCTestCase
+
+@end
+
+@implementation CatcherTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
